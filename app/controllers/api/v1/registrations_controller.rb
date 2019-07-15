d48ea541d0d3a2ea472d4210734f235759d1b1ea
Agustin Martinez <agustinmartinez@Agustins-Mac-mini.local>
2019-07-15 16:23:33 -0300

    Add permited parameters to update profile action

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      def account_update_params
+        params.permit(:name, :gender)
+      end
+
       def sign_up_params
         params.permit(:email, :password, :password_confirmation, :name, :gender)
       end
